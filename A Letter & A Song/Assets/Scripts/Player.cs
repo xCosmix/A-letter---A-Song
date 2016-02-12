17c800caeb3ab39a3f32405ac011d25fd839d7c1
Marquitos <hithere.immarcos@gmail.com>
2016-02-12 00:18:06 -0300

    cxasf

--- A Letter & A Song/Assets/Scripts/Player.cs
+++ A Letter & A Song/Assets/Scripts/Player.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class Player : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+namespace Entity
+{
+    [RequireComponent(typeof(BoxCollider2D), typeof(Rigidbody2D))]
+    public class Entity : MonoBehaviour
+    {
+        
+    }
 }
